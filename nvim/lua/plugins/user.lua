2ca2f27fe8940167b6cb5fcd5c6c2ad644c60ebd
Miguel Michel <mmichel@dropbox.com>
2026-03-12 07:35:10 +0000

    fix shuf not found on macOS

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -5,7 +5,9 @@ return {
     opts = function(_, opts)
       local fortune = vim.fn.exepath("fortune") ~= "" and "fortune" or "/usr/games/fortune"
       local cowsay = vim.fn.exepath("cowsay") ~= "" and "cowsay" or "/usr/games/cowsay"
-      local cmd = fortune .. " -s 2>/dev/null | " .. cowsay .. " -f $(" .. cowsay .. " -l | tail -n+2 | tr ' ' '\\n' | shuf -n1) 2>/dev/null"
+      local shuf = "shuf -n1"
+      if vim.fn.executable("shuf") == 0 then shuf = "sort -R | head -1" end
+      local cmd = fortune .. " -s 2>/dev/null | " .. cowsay .. " -f $(" .. cowsay .. " -l | tail -n+2 | tr ' ' '\\n' | " .. shuf .. ") 2>/dev/null"
       local header = vim.fn.system(cmd) or ""
       header = header:gsub("%s+$", "")
 
