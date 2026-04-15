bf0ef1b09333d7f96b8a2b97cb24a6b8fdf877f0
Miguel Michel <mmichel@dropbox.com>
2026-04-15 18:00:43 +0000

    obsidian.nvim: add keymaps, use Snacks picker for search

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -5,6 +5,13 @@ return {
     lazy = true,
     ft = "markdown",
     dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>of", function() Snacks.picker.files({ dirs = { "~/notes" } }) end, desc = "Find note" },
+      { "<leader>os", function() Snacks.picker.grep({ dirs = { "~/notes" } }) end, desc = "Search notes" },
+      { "<leader>ot", "<cmd>ObsidianToday<cr>", desc = "Today's note" },
+      { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "Backlinks" },
+      { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "New note" },
+    },
     opts = {
       workspaces = {
         { name = "notes", path = "~/notes" },
